84164142d227df87a5834cd2fa12153177a5c490
AllaKop <allchenok020894@gmail.com>
2024-06-13 22:37:51 +0300

    Errors handling

--- Task_2/input_output_files_preprocessor.py
+++ Task_2/input_output_files_preprocessor.py
@@ -17,31 +17,31 @@ class PdfToImageConvertor:
 
         images = convert_from_path(self.input_file)
         for i, image in enumerate(images):
-            image.save(f'{output_folder}/page_{i + 1}.png')
+            image_of_pdf = image.save(f'{output_folder}/page_{i + 1}.png')
+        return image_of_pdf
+        
 
 class ImagePreProcessor:
    
     # initializing output image
     def __init__(self, input_file):
-        self.input_file = input_file
+        self.input_file = PdfToImageConvertor()
 
-    # savinf output image
-    def saving_processed_image(self, input_file, output_folder):
-        
-        def preprocess_image(self):
-            from data_preprocessor import ImageSkewCorrector, Binarization, NoiseRemoval
+    # saving output image
+    def saving_processed_image(self, input_file, output_folder) :
+        from data_preprocessor import ImageSkewCorrector, Binarization, NoiseRemoval
 
-            # Correct Skew
-            skew_corrector = ImageSkewCorrector(self.input_file)
-            corrected_image = skew_corrector.correct_skew()
+        # Correct Skew
+        skew_corrector = ImageSkewCorrector(self.input_file)
+        corrected_image = skew_corrector.correct_skew()
 
-            # Convert to Gray and Binarize
-            binarizer = Binarization(corrected_image)
-            gray_image = binarizer.gray_conversion()
-            binarized_image = binarizer.binarized_conversion(gray_image)
+        # Convert to Gray and Binarize
+        binarizer = Binarization(corrected_image)
+        gray_image = binarizer.gray_conversion()
+        binarized_image = binarizer.binarized_conversion(gray_image)
 
-            # Remove Noise
-            noise_removal = NoiseRemoval(binarized_image)
-            final_image = noise_removal.gaussian_blurring(binarized_image)
+        # Remove Noise
+        noise_removal = NoiseRemoval(binarized_image)
+        final_image = noise_removal.gaussian_blurring(binarized_image)
 
-            return final_image
+        return final_image
